ce0e252378cc35cda20223ceb653cf6772e70fae
Shay <shay.teaching@gmail.com>
2021-07-01 20:38:31 +0300

    Basic solution skeleton

---
+++ tennis/tennis_game_class.py
@@ -0,0 +1,9 @@
+
+class Tennis:
+    def __init__(self):
+        self.group_1_score = 0
+        self.group_2_score = 0
+
+
+
+
---
+++ tennis/test_tennis.py
@@ -0,0 +1,8 @@
+import unittest
+
+
+class TestStringMethods(unittest.TestCase):
+    def test_something(self):
+        pass
+
+
