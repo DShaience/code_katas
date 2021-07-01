94a1c3c5054d4211d42372c694ca1f92f022e84b
Shay <shay.teaching@gmail.com>
2021-07-01 21:07:33 +0300

    implemented parameterized test

--- tennis/test_tennis.py
+++ tennis/test_tennis.py
@@ -1,8 +1,21 @@
 import unittest
+from tennis_game_class import Tennis
+from parameterized import parameterized
 
 
 class TestStringMethods(unittest.TestCase):
-    def test_something(self):
-        pass
+    def setUp(self) -> None:
+        self.game = Tennis()
+
+    @parameterized.expand([
+        [0, 4, True],
+        [1, 4, True],
+        [2, 3, False],
+        [4, 3, False],
+    ])
+    def test_is_victory(self, score_1, score_2, expected):
+        self.assertEqual(self.game.is_victory(score_1, score_2), expected)
+
+
 
 
