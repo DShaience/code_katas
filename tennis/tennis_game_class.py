f1c6c42a9f253a4adfc96c6bb8523e62c54e3ced
Shay <shay.teaching@gmail.com>
2021-07-01 21:07:56 +0300

    moved constants to class instead of an instance attribute

--- tennis/tennis_game_class.py
+++ tennis/tennis_game_class.py
@@ -1,8 +1,14 @@
 
 class Tennis:
+    MIN_VICTORY_SCORE = 4
+
     def __init__(self):
         self.group_1_score = 0
         self.group_2_score = 0
+
+    def is_victory(self, score_1: int, score_2: int):
+        return (abs(score_1 - score_2) >= 2) and any([score >= self.MIN_VICTORY_SCORE for score in [score_1, score_2]])
+
 
 
 
